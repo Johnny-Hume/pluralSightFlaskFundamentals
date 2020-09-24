d22567ccf96fbf1fddb935ee11b9cc224dca722c
Johnny <jhume04@qub.ac.uk>
2020-09-24 07:33:44 +0100

    Removed date page

--- flashcards.py
+++ flashcards.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from datetime import datetime
 
 from model import db
 
@@ -14,16 +13,11 @@ def welcome():
         )
 
 
-@app.route("/date")
-def date():
-    return "This page was served at "+str(datetime.now())
-
-
 numberOfTimes = 0
 
 
-@app.route("/stalker")
-def stalker():
+@app.route("/count")
+def countNumberOfViews():
     global numberOfTimes
     numberOfTimes += 1
     return "This page has been viewed " + str(numberOfTimes) + " times!"
